d45ac8bbb94409d847cf135599a918b023b72d55
Ignacio Cisternas <nachocisternas95@gmail.com>
2024-06-09 23:58:04 -0400

    full flow from menu to dificult and game

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,42 @@
 import React from "react";
+import "./Modal.css";
+import { FaTemperatureQuarter } from "react-icons/fa6";
+import { FaTemperatureHalf } from "react-icons/fa6";
+import { FaTemperatureFull } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 export function Modal() {
-  return <span>modal</span>;
+  const navigate = useNavigate();
+  return (
+    <div className="modalContainer">
+      <div className="titleContainer">
+        <span className="modalTitle">
+          What difficulty would you like to play at?
+        </span>
+      </div>
+      <div className="levelsContainer">
+        <div
+          className="dificultContainer dificultContainerEasy"
+          onClick={() => navigate("/Game")}
+        >
+          <FaTemperatureQuarter size={100} fill="green" />
+          <span className="dificultText"> Easy</span>
+        </div>
+        <div
+          className="dificultContainer dificultContainerMedium"
+          onClick={() => navigate("/Game")}
+        >
+          <FaTemperatureHalf size={100} fill="yellow" />
+          <span className="dificultText"> Medium</span>
+        </div>
+        <div
+          className="dificultContainer dificultContainerHard"
+          onClick={() => navigate("/Game")}
+        >
+          <FaTemperatureFull size={100} fill="red" />
+          <span className="dificultText"> Hard</span>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/screens/Menu/Menu.tsx
+++ src/screens/Menu/Menu.tsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Menu.css";
 import { FaPlay } from "react-icons/fa";
 import { TfiCup } from "react-icons/tfi";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button/Button.tsx";
+import { Modal } from "../../components/Modal/Modal.tsx";
 
 function menuCard() {
   const navigate = useNavigate();
+  const [openModal, setOpenModal] = useState(false);
 
   const menuButtons = [
     {
       title: "Start Quiz",
       icon: <FaPlay size={15} fill="white" />,
       type: "Primary",
-      onClick: () => navigate("/Game"),
+      onClick: () => setOpenModal(true),
     },
     {
       title: "High Scores",
@@ -25,6 +27,7 @@ function menuCard() {
 
   return (
     <div className="cardContainer">
+      {openModal && <Modal />}
       <h2>World Cup Party Quiz</h2>
       <h3>{"subtitle"}</h3>
 
