a8ce29b53410c445b243a41a19b0f6c4a846eb72
Ignacio Cisternas <nachocisternas95@gmail.com>
2024-06-13 21:29:54 -0400

    modal options on map

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,9 +4,44 @@ import { FaTemperatureQuarter } from "react-icons/fa6";
 import { FaTemperatureHalf } from "react-icons/fa6";
 import { FaTemperatureFull } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
+import { useScore } from "../../context/ScoreContext.tsx";
+
+const dificultsOptions = [
+  {
+    title: "Easy",
+    icon: <FaTemperatureQuarter size={100} fill="green" />,
+  },
+  {
+    title: "Medium",
+    icon: <FaTemperatureHalf size={100} fill="yellow" />,
+  },
+  {
+    title: "Hard",
+    icon: <FaTemperatureFull size={100} fill="red" />,
+  },
+];
 
 export function Modal() {
   const navigate = useNavigate();
+  const { resetTime } = useScore();
+
+  const clickHandler = () => {
+    resetTime();
+    navigate("/Game");
+  };
+
+  const optionRender = (option) => {
+    return (
+      <div
+        className={`dificultContainer dificultContainer${option.title}`}
+        onClick={() => clickHandler()}
+      >
+        {option.icon}
+        <span className="dificultText"> {option.title}</span>
+      </div>
+    );
+  };
+
   return (
     <div className="modalContainer">
       <div className="titleContainer">
@@ -15,27 +50,9 @@ export function Modal() {
         </span>
       </div>
       <div className="levelsContainer">
-        <div
-          className="dificultContainer dificultContainerEasy"
-          onClick={() => navigate("/Game")}
-        >
-          <FaTemperatureQuarter size={100} fill="green" />
-          <span className="dificultText"> Easy</span>
-        </div>
-        <div
-          className="dificultContainer dificultContainerMedium"
-          onClick={() => navigate("/Game")}
-        >
-          <FaTemperatureHalf size={100} fill="yellow" />
-          <span className="dificultText"> Medium</span>
-        </div>
-        <div
-          className="dificultContainer dificultContainerHard"
-          onClick={() => navigate("/Game")}
-        >
-          <FaTemperatureFull size={100} fill="red" />
-          <span className="dificultText"> Hard</span>
-        </div>
+        {dificultsOptions.map((option) => {
+          return optionRender(option);
+        })}
       </div>
     </div>
   );
